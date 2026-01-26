d8cef99a3a1dd79081b2930527c92eec025d4905
Siddiqkolimi <104221239+Siddiq3@users.noreply.github.com>
2026-01-26 11:58:10 +0530

    Change cheerio import to namespace import

--- scrapeJobs.js
+++ scrapeJobs.js
@@ -1,7 +1,6 @@
 import fs from "fs";
 import fetch from "node-fetch";
-import cheerio from "cheerio";
-
+import * as cheerio from "cheerio";
 async function scrapeIndeed() {
   const url = "https://in.indeed.com/jobs?q=software+developer&l=India";
 
